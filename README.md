af9d2c7e1615ad014a89f622fc94dcc7103d60ec
Brady Slot <bradyslot@gmail.com>
2023-07-27 16:48:06 +1000

    convert into api resource for classes and equipment

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dungeons & Dragons 5e SRD Lightweight API
+
+- Used in conjunction with [open5e-api](https://github.com/open5e/open5e-api)
+- Supplies classes and equipment from the 5e standard reference document
+- Api resource for [DnDex](https://github.com/bradyslot/dndex)
--- src/main.rs
+++ src/main.rs
@@ -3,103 +3,24 @@
 
 use rocket::serde::{Deserialize, Serialize};
 use rocket::serde::json::{Json, json, Value};
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::io::Read;
-use std::fs::File;
-use md5::{Digest, Md5};
-use std::io;
-use std::fs::{self, DirEntry};
-use std::path::Path;
-use std::cell::RefCell;
-use chrono::{DateTime, NaiveDateTime, Utc};
 use lazy_static::lazy_static;
 
-const DATA_DIR: &str = "data";
-
-#[derive(Serialize, Clone)]
-#[serde(crate = "rocket::serde")]
-struct Manifest {
-    filename: String,
-    hash: String,
-    created_at: String,
-}
-
-fn md5_hash(file_path: &Path) -> String {
-    let mut file = File::open(file_path).unwrap();
-    let mut hasher = Md5::new();
-    let mut buffer = [0; 8192];
-
-    loop {
-        let bytes_read = file.read(&mut buffer).unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-        hasher.update(&buffer[..bytes_read]);
-    }
-
-    let hash_result = hasher.finalize();
-    let hash_string = format!("{:x}", hash_result);
-
-    hash_string
-}
-
-fn timestamp(path: &Path) -> String {
-    let created = fs::metadata(path)
-        .unwrap()
-        .created()
-        .unwrap()
-        .duration_since(std::time::SystemTime::UNIX_EPOCH)
-        .unwrap();
-
-    let naive_datetime = NaiveDateTime::from_timestamp_opt(created.as_secs() as i64, created.subsec_nanos())
-        .expect("Invalid timestamp");
-    let datetime_utc: DateTime<Utc> = DateTime::from_utc(naive_datetime, Utc);
-
-    datetime_utc.format("%Y-%m-%dT%H:%M:%S%.6f").to_string()
-}
-
-fn file_info(path: &Path) -> Manifest {
-    Manifest {
-        filename: path.to_str().unwrap().to_string(),
-        hash: md5_hash(path),
-        created_at: timestamp(path),
-    }
-}
-
-fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry);
-            }
-        }
-    }
-    Ok(())
-}
-
 lazy_static! {
-    static ref MANIFEST_CACHE: Vec<Manifest> = {
-        let manifest: RefCell<Vec<Manifest>> = RefCell::new(Vec::new());
-
-        visit_dirs(Path::new(DATA_DIR), &|entry: &DirEntry| {
-            manifest.borrow_mut().push(file_info(&entry.path()));
-        })
-        .unwrap();
-
-        manifest.into_inner()
+    static ref BARBARIAN_CACHE: Value = {
+        let path = "data/barbarian.json";
+        let file = std::fs::File::open(path).unwrap();
+        serde_json::from_reader(file).unwrap()
     };
 }
 
-#[get("/manifest")]
-fn get_manifest() -> Value {
-    json!(&*MANIFEST_CACHE)
+#[get("/barbarian")]
+fn get_barbarian() -> Value {
+    json!(&*BARBARIAN_CACHE)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_manifest])
+    rocket::build().mount("/", routes![
+        get_barbarian,
+    ])
 }
